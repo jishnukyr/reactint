e78e24aad7235ffa166c5e786ee969b68125f67d
jishnukyr <jishnu6753@gmail.com>
2023-07-01 11:17:05 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import image from '../assets/pic.jpg'
+export const Home = () => {
+  return (
+<div className="nav">
+    <div className="image">   
+    <img src={image} alt="" />
+   </div>
+   <div className="a">premium</div>
+   <div className="b">help</div>
+    <div className="c"></div>
+
+
+
+</div>
+
+  
+  )
+}
+export default Home
