e946db56bff046495889c2392eade445366b5c39
jishnukyr <jishnu6753@gmail.com>
2023-07-04 16:32:10 +0530

    jishnu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
-
+import './Home.css'
 import image from '../assets/pic.jpg'
 export const Home = () => {
   return (
+    <>
 <div className="nav">
-    <div className="image">   
-    <img src={image} alt="" />
-   </div>
+        <div className="image">   
+        <img src={image} alt="" />
+        </div>
    <div className="a">premium</div>
    <div className="b">help</div>
-    <div className="c">jishnu</div>
-    <div className="d">kayyur</div>
-
-
-
+    <div className="c">Download</div>
+    <div className="d">Sign Up</div>
+    <div className="e">Log In</div>
 </div>
+          <div className="con">
+          <div className="head">Music for everyone</div>
+          <div className="sub">Millions of songs. No creadit card needed</div>
+          <div className="button">
+            <button>GET SPOTIFY FREE</button>
+          </div>
+          </div>
 
-  
+          </>
   )
 }
 export default Home
