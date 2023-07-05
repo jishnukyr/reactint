24d78513a15eb67db94fd730cdb5e6b3be5df6ca
jishnukyr <jishnu6753@gmail.com>
2023-07-05 16:25:09 +0530

    jishnu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Home.css'
 import image from '../assets/pic.jpg'
 export const Home = () => {
@@ -12,11 +13,14 @@ export const Home = () => {
    <div className="b">help</div>
     <div className="c">Download</div>
     <div className="d">Sign Up</div>
+    <Link to={"/Login"} id='lg' >
+      
     <div className="e">Log In</div>
+    </Link>
 </div>
           <div className="con">
           <div className="head">Music for everyone</div>
-          <div className="sub">Millions of songs. No creadit card needed</div>
+          <div className="sub">Millions of songs. No creadit card needed</div><br />
           <div className="button">
             <button>GET SPOTIFY FREE</button>
           </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
-  return (
-    <div>Login</div>
+    return (
+        <>
+
+            <div className='lpg'>
+                <div className='frm'>
+                    <form id='lgin'>
+                        <h2>Login</h2>
+                        <label htmlFor="email">Email:</label><br />
+                        <input type="email" placeholder='Enter email' id='email' /><br /><br />
+                        <label htmlFor="pass">Password:</label><br />
+                        <input type="password" placeholder="Enter password"id='pass' /><br /><br />
+                        <input type="checkbox" id="show" />
+                        <label htmlFor="show">Show password</label><br /><br />
+                        <button>SIGN IN</button><br /><br />
+                        <div className="fp">Forgot <a href="none">Username/Password</a></div><br />
+                        <div className="da">Dont have an account ? <a href="none">Sign up</a></div>
+                    </form>
+                </div>
+            </div>
+
+        </>
   )
 }
 
